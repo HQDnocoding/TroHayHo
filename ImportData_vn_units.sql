6581fa8afeb45cf99ccdabe3f4c2ff76d6523345
HyHy1506 <tran1106duc@gmail.com>
2025-01-09 17:15:42 +0700

    chinh lai sql

--- ImportData_vn_units.sql
+++ ImportData_vn_units.sql
@@ -3,11 +3,11 @@
 /* Reference: https://github.com/ThangLeQuoc/vietnamese-provinces-database */
 /* =============================================== */
 use timtrodb;
-DELETE FROM wards;
-DELETE FROM districts;
-DELETE FROM provinces;
-DELETE FROM administrative_units;
-DELETE FROM administrative_regions;
+-- DELETE FROM wards;
+-- DELETE FROM districts;
+-- DELETE FROM provinces;
+-- DELETE FROM administrative_units;
+-- DELETE FROM administrative_regions;
 -- DATA for administrative_regions --
 INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(1,'Đông Bắc Bộ','Northeast','dong_bac_bo','northest');
 INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(2,'Tây Bắc Bộ','Northwest','tay_bac_bo','northwest');
